4b120970cf8ba3b57a890c6c39316c9377de1c02
Tom Solberg <me@sbg.dev>
2023-08-25 19:41:43 +0200

    dep inference failure in 2.17.0rc4

---
+++ dep-inference-217/cmd/decimate.py
@@ -0,0 +1,7 @@
+"""
+
+"""
+from tensorboard.backend.event_processing import event_accumulator
+from tensorboard.compat.proto import types_pb2
+from tensorboard.compat.proto.summary_pb2 import HistogramProto, Summary
+from torch.utils.tensorboard import SummaryWriter
---
+++ dep-inference-217/cmd/test_decimate.py
@@ -0,0 +1,4 @@
+from tensorboard.backend.event_processing import event_accumulator
+from torch.utils.tensorboard import SummaryWriter
+
+import decimate
